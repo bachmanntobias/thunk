0762cc9f4e14ef8d73a12c86cf60ffac947fcbd7
Tobias Bachmann <bachmanntobias96@gmail.com>
2018-02-28 15:05:06 +0100

    11

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 export function fetchUsers() {
-const request = axios.get('http://jsonplaceholder.typicode.com/users');
+  const request = axios.get('http://jsonplaceholder.typicode.com/users');
 
+  return(dispatch) => {
+    request.then(({data}) => {
+      dipatch({type: 'FETCH_PROFILES', payload: data})
+    });
+  };
 }
