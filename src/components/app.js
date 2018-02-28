cbe106ecc10f8d3ac6f6bdbfa5daa8f26f830698
Tobias Bachmann <bachmanntobias96@gmail.com>
2018-02-28 12:26:19 +0100

    mittagspause

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
 export default class App extends Component {
-  render() {
+  componentWillmount() {
+      this.props.fetchUsers();
+  }
+
+  renderUser({id, name, email}) {
     return (
-      <div>React simple starter</div>
+      <div>
+<li className="list-group-item" key={id}>
+<span className="label label-default label-pill pull-xs-right">
+<a href={email}>{email}</a>
+</span>
+{name}
+</li>
+      </div>
     );
   }
+render() {
+  return (
+    <div>
+      <h4>Hi</h4>
+<ul className="list-group">
+  {this.props.users.map(this.renderUser)}
+</ul>
+</div>
+  )
+}
+
 }
